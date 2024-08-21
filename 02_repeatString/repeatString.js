be8e9857e18f54f9cf49977e6ddf70921827a8d4
Glorious-Goodness Oyetunji <gloriousgoodness441@gmail.com>
2024-08-20 20:36:32 -0400

    Finished exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, repeatNumber) {
+    let repeatedString = "";
+    if(repeatNumber >= 0){
+        for(i = 0 ; i < repeatNumber ; i++){
+            repeatedString += string;
+        }
+     }
 
+     else{
+        repeatedString = "ERROR"
+     }
+    return repeatedString;
 };
 
 // Do not edit below this line
