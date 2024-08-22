f5e4a1f3b4169f94147945f04a6654db8162525e
Glorious-Goodness Oyetunji <gloriousgoodness441@gmail.com>
2024-08-22 11:02:45 -0400

    Finsished exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+ var baseArray = arguments[0];
+ var args = Array.from(arguments).slice(1);
+ var newArray;
+
+ for(i = baseArray.length - 1; i >= 0; i--){
+ for (j = 0; j < args.length ; j++){
+        if(baseArray[i] === args[j]){
+            baseArray.splice(i,1);
+        }
+    }
+ }
+
+ return baseArray;
 };
+
+console.log(removeFromArray([1,2,3,4], 3));
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
