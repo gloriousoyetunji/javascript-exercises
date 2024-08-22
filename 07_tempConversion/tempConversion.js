c0c5b942a9986080e8211221da95629eacb8a41f
Glorious-Goodness Oyetunji <gloriousgoodness441@gmail.com>
2024-08-22 14:57:30 -0400

    Completed exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperatureInFarenheit) {
+  // return ((temperatureInFarenheit - 32) * (5/9));
+  return Math.round(((temperatureInFarenheit - 32) * (5/9)) * 10) / 10 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperatureInCelcius) {
+  // return ((temperatureInCelcius * (9/5)) + 32);
+  return Math.round(((temperatureInCelcius * (9/5)) + 32) * 10) / 10 ;
 };
 
 // Do not edit below this line
