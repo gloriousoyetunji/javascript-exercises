254c0b258b53ebd5a14adaba120c2e7f2179036e
Glorious-Goodness Oyetunji <gloriousgoodness441@gmail.com>
2024-08-22 14:36:43 -0400

    Completed exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    var isleapYear = false;
 
+    if(((year % 4) == 0) && (((year % 100) != 0) || ((year % 400) == 0)) ){
+        isleapYear = true;
+    }
+
+    return isleapYear;
 };
 
 // Do not edit below this line
